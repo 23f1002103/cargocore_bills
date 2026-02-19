2340dda0e95df5de698748f1cc8c9d65660f88ed
Pruthvi Prasad <pruthviprasad280@gmail.com>
2026-02-19 19:16:16 +0530

    Convert configuration to JS and fix Netlify config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [
